5988a8579132588eae22beaa98ae25dadeae73dc
한상선 <hs94122@naver.com>
2023-05-12 20:42:16 +0900

    수동 테스트를 Junit5로 리팩토링

---
+++ src/test/kotlin/com/group/libraryapp/JunitTest.kt
@@ -0,0 +1,27 @@
+package com.group.libraryapp
+
+import org.junit.jupiter.api.Test
+
+/**
+ * packageName  : com.group.libraryapp
+ * fileName     : JunitTest
+ * author       : sshan
+ * date         : 2023-05-12
+ * description  :
+ * ========================================================
+ *  DATE            AUTHOR              NOTE
+ * --------------------------------------------------------
+ * 2023-05-12          sshan            최초생성
+ */
+class JunitTest {
+
+    @Test
+    fun test1(){
+        println("테스트1")
+    }
+
+    @Test
+    fun test2(){
+        println("테스트2")
+    }
+}
