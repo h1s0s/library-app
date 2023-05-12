2af25868af32a0d5d7a4316c326a1bae296908a3
한상선 <hs94122@naver.com>
2023-05-12 20:25:59 +0900

    수동 테스트 작성

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -14,6 +14,10 @@ package com.group.libraryapp.calculator
 fun main(){
     val calculatorTest = CalculatorTest()
     calculatorTest.addTest()
+    calculatorTest.minusTest()
+    calculatorTest.multiplyTest()
+    calculatorTest.divideTest()
+    calculatorTest.divideExceptionTest()
 }
 
 class CalculatorTest {
@@ -28,5 +32,64 @@ class CalculatorTest {
         if (calculator.number != 8){
             throw IllegalArgumentException()
         }
+    }
+
+    fun minusTest(){
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.minus(3)
+
+        // then
+        if (calculator.number != 2){
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun multiplyTest(){
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.multiply(3)
+
+        // then
+        if (calculator.number != 15){
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun divideTest(){
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.divide(2)
+
+        // then
+        if (calculator.number != 2){
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun divideExceptionTest(){
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        try {
+            calculator.divide(0)
+        } catch (e:IllegalArgumentException){
+            //테스트 성공
+            if(e.message != "0으로 나눌 수 없습니다"){
+                throw IllegalStateException("메시지가 다릅니다")
+            }
+            return
+        } catch (e: Exception){
+            throw IllegalStateException()
+        }
+        //then
+        throw IllegalStateException("기대하는 예외가 발생하지 않았습니다")
     }
 }
