9065a5e9dbaf636963023c1d677f96b60a3dd9b3
h1s0s <hs94122@naver.com>
2023-05-16 17:35:58 +0900

    공부

--- src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -7,15 +7,31 @@ import javax.persistence.Id
 
 @Entity
 class Book (
-    val name : String,
+    val name: String,
+
+    val type: BookType,
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id : Long? = null,
+    val id: Long? = null,
 ){
     init{
         if(name.isBlank()){
             throw IllegalArgumentException("이름은 비어 있을 수 없습니다")
+        }
+    }
+
+    companion object{
+        fun fixture(
+            name: String = "책 이름",
+            type: BookType = BookType.COMPUTER,
+            id: Long? = null
+        ): Book{
+            return Book(
+                name = name,
+                type = type,
+                id = id,
+            )
         }
     }
 }
--- src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -2,6 +2,7 @@ package com.group.libraryapp.service.book
 
 import com.group.libraryapp.domain.book.Book
 import com.group.libraryapp.domain.book.BookRepository
+import com.group.libraryapp.domain.book.BookType
 import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.book.request.BookLoanRequest
@@ -35,7 +36,7 @@ class BookServiceTest @Autowired constructor(
     @DisplayName("책 등록이 정상 동작한다.")
     fun saveBookTest(){
         //given
-        val request = BookRequest("이상한 나라의 앨리스")
+        val request = BookRequest("이상한 나라의 앨리스", BookType.COMPUTER)
 
         //when
         bookService.saveBook(request)
@@ -50,7 +51,7 @@ class BookServiceTest @Autowired constructor(
     @DisplayName("책 대출이 정상 동작한다.")
     fun loanBookTest(){
         //given
-        bookRepository.save(Book("이상한 나라의 앨리스"))
+        bookRepository.save(Book.fixture("이상한 나라의 앨리스"))
         val savedUser = userRepository.save(User("최태현", null))
         val request = BookLoanRequest("최태현", "이상한 나라의 앨리스")
 
@@ -69,7 +70,7 @@ class BookServiceTest @Autowired constructor(
     @DisplayName("책이 진작 대출되어 있다면, 신규 대출이 실패한다.")
     fun loanBookFailTest(){
         //given
-        bookRepository.save(Book("이상한 나라의 앨리스"))
+        bookRepository.save(Book.fixture("이상한 나라의 앨리스"))
         val savedUser = userRepository.save(User("최태현", null))
         userLoanHistoryRepository.save(UserLoanHistory(savedUser, "이상한 나라의 앨리스", false))
         val request = BookLoanRequest("최태현", "이상한 나라의 앨리스")
@@ -85,7 +86,7 @@ class BookServiceTest @Autowired constructor(
     @DisplayName("책 반납이 정상 동작한다.")
     fun returnBookTest() {
         //given
-        bookRepository.save(Book("이상한 나라의 앨리스"))
+        bookRepository.save(Book.fixture("이상한 나라의 앨리스"))
         val savedUser = userRepository.save(User("최태현", null))
         userLoanHistoryRepository.save(UserLoanHistory(savedUser, "이상한 나라의 앨리스", false))
         val request = BookReturnRequest("최태현", "이상한 나라의 앨리스")
